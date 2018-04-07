59796b72af9d82e0b4270466846ca4318a0eb53a
Ayrton Gomes <com.ayrton@gmail.com>
2018-04-07 11:29:46 -0100

    working on stuff

--- tx-cli.ts
+++ tx-cli.ts
@@ -1,7 +1,10 @@
 const WavesAPI = require('waves-api');
+
 WavesAPI.TESTNET_CONFIG.nodeAddress = 'http://127.0.0.1:6861';
 WavesAPI.TESTNET_CONFIG.matcherAddress = 'http://127.0.0.1:6862';
+
 const Waves = WavesAPI.create(WavesAPI.TESTNET_CONFIG);
+
 function parseArguments() {
     const result = Object.create(null);
     process.argv.forEach((argument) => {
@@ -10,27 +13,32 @@ function parseArguments() {
             const name = argument.substr(0, index);
             const value = argument.substr(index + 1);
             result[name] = value;
-        }
-        else {
+        } else {
             result[argument] = true;
         }
-    });
+    });    
     return result;
 }
-const args = parseArguments();
+
+const args=parseArguments();
+
 console.info(args);
+
 if (!args.to || !args.amount) {
     console.info("Usage: tx-cli <to> <value>");
-    process.exit();
+    process.exit()
 }
+
 const keyPair = {
     privateKey: 'EXKwLZybgit3uKddrMDNBXXES4P2prUmMPqHMWpyY1V5',
-    publicKey: '3tWuqg9syHTsdmNNmwUbguLUhnpyE5AS4rpkojgm6aw2'
+    publicKey: '3tWuqg9syHTsdmNNmwUbguLUhnpyE5AS4rpkojgm6aw2' 
 };
-let attach = '';
+
+let attach='';
 if (args.attach) {
     attach = args.attach;
 }
+
 // 10070000000
 const tx = {
     // recipient: '3NNQxWojKFQT1gtR6fqefbq1hpwPijkDG2P',
@@ -42,7 +50,9 @@ const tx = {
     attachment: attach,
     timestamp: Date.now()
 };
+
 console.info("Sending %d %s to %s", tx.amount, tx.assetId, tx.recipient);
+
 Waves.API.Node.v1.assets.transfer(tx, keyPair).then((responseData) => {
     console.log("Your Transaction has been sent");
     console.log("Details: ", responseData);
@@ -50,4 +60,3 @@ Waves.API.Node.v1.assets.transfer(tx, keyPair).then((responseData) => {
     console.error("Your Transaction could not be sent");
     console.error("Details: ", error);
 });
-//# sourceMappingURL=tx-cli.js.map
